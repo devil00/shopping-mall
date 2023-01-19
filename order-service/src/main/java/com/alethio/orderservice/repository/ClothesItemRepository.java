5945a35561c492bed818cfbdd9343c47aade83a4
Mayur Swami <swamimayur0@gmail.com>
2023-01-19 10:36:00 +0530

    removed unused files

--- order-service/src/main/java/com/alethio/orderservice/repository/ClothesItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alethio.orderservice.repository;
-
-import com.alethio.orderservice.model.ClothesItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClothesItemRepository extends JpaRepository<ClothesItem, Integer> {
-}
